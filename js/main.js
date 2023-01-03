97306a8af9afb7922485c0f3f5c5badd2062959b
Daniel <danielschung@icloud.com>
2023-01-03 05:34:39 -0600

    json test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 console.log('works');
 
-// fetch('https://danielschung.github.io/covenantlove/js/data.json')
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+function getData() {
+	fetch('https://danielschung.github.io/covenantlove/js/data.json', {
+		method: 'GET'
+	})
+	.then((response) => response.json())
+	.then((data) => console.log(data));
+}
+
+getData();
+
