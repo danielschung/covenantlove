3ffaf27278459aeb519807c581ff2aa3df32a8e5
Daniel <danielschung@icloud.com>
2023-01-05 23:48:12 -0600

    mobile changes, and search styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('works');
+console.log('main.js works');
+
+
+// ------- 01 Mobile Nav -------
+
+function mobileNav() {
+	let navButton = document.getElementById('mobile-nav');
+	let links = navButton.previousElementSibling;
+
+	if ( !$(navButton).hasClass('x') ) {
+		$(navButton).addClass('x');
+		$(links).fadeIn().animate({ left:'0' }, '200ms');
+	} else {
+		$(navButton).removeClass('x');
+		$(links).fadeIn().animate({ left:'100vw' }, '200ms');
+	}
+}
+
+
+// ------- 02 Search -------
+const searchInput = document.getElementById('search');
+
+// search query
+$(searchInput).keypress(function(event){
+	if( event.keyCode === 13) {
+		searchInput.blur();
+		let value = searchInput.value;
+		console.log(value + ' search queried...');
+	}
+})
+
+
