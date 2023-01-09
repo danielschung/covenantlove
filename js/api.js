a3504f9aed54c954fc320b57a96e70917e0c84c2
Daniel <danielschung@icloud.com>
2023-01-09 17:00:59 -0600

    updated data pull and json

--- js/api.js
+++ js/api.js
@@ -1,4 +1,7 @@
+console.log('api.js works');
+
 const url = 'https://danielschung.github.io/covenantlove/js/data.json';
+const cardContainer = document.getElementById('results').childNodes[1];
 
 const getData = async (request) => {
 	try {
@@ -10,28 +13,45 @@ const getData = async (request) => {
 			return;
 		}
 
-		let id = data.sermons[0].id;
-		let passage = data.sermons[0].passage;
-		let title = data.sermons[0].title;
-		let speaker = data.sermons[0].speaker;
-		let date = data.sermons[0].date;
-		let plays = data.sermons[0].plays;
-		let tags = data.sermons[0].tags;
+		let num = data.sermons.length;
+		
+		for (let i = 0; i < num; i++) {
 
-		console.log(passage);
+			let id = data.sermons[i].id;
+			let passage = data.sermons[i].passage;
+			let title = data.sermons[i].title;
+			let speaker = data.sermons[i].speaker;
+			let date = data.sermons[i].date;
+			let plays = data.sermons[i].plays;
+			let tags = data.sermons[i].tags;
+			let time = data.sermons[i].time;
 
-		let cards = document.getElementsByClassName('card');
-		let dataPoints = cards[0].children;
-		// console.log(dataPoints);
+			let block = `
+				<div class="card">
+					<div class="top">
+						<div class="speaker">${speaker}</div>
+						<div class="date">${date}</div>
+					</div>
+					<div class="middle">
+						<h2 class="title">${title}</h2>
+						<h3 class="passage">${passage}</h3>
+					</div>
+					<div class="bottom">
+						<div class="time">0:00 | ${time}</div>
+						<div class="plays">${plays} plays</div>
+					</div>
+				</div>
+			`;
 
-		// cards[0].innerHTML = value
+			cardContainer.innerHTML += block;
+		}
 
 	} catch(error) {
 		console.log(error);
 	}
 }
 
-const createCards {
+function createCards() {
 	//write code to create cards after you figure out css
 }
 
